1a3f0d709244edb64d03cea61310687cae108aef
Mateusz Fabisiak <mateusz.fabisiak.87@gmail.com>
2022-02-28 17:53:44 +0100

    UPDATE OrderOptionText

---
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './OrderOption.module.scss';
+import PropTypes from 'prop-types';
+
+
+const OrderOptionText = ({ setOptionValue, currentValue }) => {
+  return (
+    <input
+      type="text"
+      value={currentValue}
+      className={styles.input}
+      onChange={event => setOptionValue(event.currentTarget.value)}
+      placeholder='enter text'
+    />
+
+  );
+};
+
+OrderOptionText.propTypes = {
+  setOptionValue: PropTypes.func,
+  currentValue: PropTypes.string,
+};
+export default OrderOptionText;
